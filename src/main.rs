514b6a26608ecf735a34d54ac8c40209ef2f8d62
Vinicius CZB <viniczb@gmail.com>
2023-10-14 17:10:24 +0200

    create functions for to-do operations

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,27 @@ struct TodoItem {
   completed: bool,
 }
 
+fn add_todo(todos: &mut Vec<TodoItem>, title: String) {
+  let new_id = todos.len() as u32 + 1;
+  let new_todo = TodoItem {
+      id: new_id,
+      title,
+      completed: false,
+  };
+  todos.push(new_todo);
+}
+
+fn complete_todo(todos: &mut Vec<TodoItem>, id: u32) {
+  if let Some(todo) = todos.iter_mut().find(|todo| todo.id == id) {
+      todo.completed = true;
+  }
+}
+
+fn list_todos(todos: &Vec<TodoItem>) {
+  for todo in todos {
+      println!("ID: {}, Title: {}, Completed: {}", todo.id, todo.title, todo.completed);
+  }
+}
 
 fn main() {
     println!("Hello, world!");
