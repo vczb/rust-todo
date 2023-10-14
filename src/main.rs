069ab07e860550c8ecdb5cf8b523087e7fcc956d
Vinicius CZB <viniczb@gmail.com>
2023-10-14 17:36:43 +0200

    persist todo list in a json file

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,29 @@ mod commands;
 use crate::todo_item::TodoItem;
 use crate::commands::{add_todo, complete_todo, list_todos};
 
+use std::fs::File;
+use std::io::Write;
+use std::io::Read;
 
+pub fn read_from_file() -> std::io::Result<Vec<TodoItem>> {
+    let mut file = File::open("todos.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    let todos: Vec<TodoItem> = serde_json::from_str(&contents).unwrap_or_else(|_| vec![]);
+    Ok(todos)
+}
+
+pub fn save_to_file(todos: &Vec<TodoItem>) -> std::io::Result<()> {
+    let json = serde_json::to_string(&todos).unwrap();
+    let mut file = File::create("todos.json")?;
+    file.write_all(json.as_bytes())?;
+    Ok(())
+}
 
 fn main() {
-  let mut todos: Vec<TodoItem> = Vec::new();
+  
+  let mut todos = read_from_file().unwrap_or_else(|_| vec![]);
+
   loop {
       println!("What would you like to do?");
       println!("1: Add Todo");
@@ -38,5 +57,9 @@ fn main() {
           "4" => break,
           _ => println!("Invalid choice, please try again."),
       }
+  }
+
+  if let Err(err) = save_to_file(&todos) {
+    eprintln!("Error saving todos: {}", err);
   }
 }
--- src/todo_item.rs
+++ src/todo_item.rs
@@ -1,3 +1,6 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct TodoItem {
   id: u32,
   title: String,
