9c57cabb84605c16b0894ea8c81ef7a5defe5cc6
Vinicius CZB <viniczb@gmail.com>
2023-10-15 16:51:47 +0200

    set up args to choose the view type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 mod entities;
 mod repository;
 mod services;
@@ -7,7 +9,9 @@ use views::cli::run_cli;
 use views::gui::run_gui;
 
 pub fn main() {
-  if true {
+  let args: Vec<String> = env::args().collect();
+
+  if args.contains(&"--gui".to_string()) {
     run_gui();
   } else {
     run_cli();
